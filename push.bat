7e5a63649a7bbbc522b7068542a3f522d78887c8
primusr@M306.UC-BCF.EDU.PH <primusr@M306.UC-BCF.EDU.PH>
2022-09-14 14:42:16 +0800

    Auto upload

--- push.bat
+++ push.bat
@@ -1,5 +1,8 @@
 @echo off
+git remote set-url origin https://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@github.com/primusr/CIT142G.git
+pause
 git add .
 git commit -m "Auto upload"
 git push
+pause
 
